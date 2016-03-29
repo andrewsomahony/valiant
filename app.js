30d1e53400ddf857cb8adaeca950882f473e3e8c
Andrew O'Mahony <aomahony@artlogic.com>
2016-03-29 08:42:42 +0200

    Active/Inactive server variable

--- app.js
+++ app.js
@@ -17,70 +17,91 @@ var appConfig = require('./config/config');
 
 var connectDb = require('./db/connect');
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+var activeVariable = 'EL_THATIS_SATURN';
+var appIsActive = true;
 
-app.disable('view cache');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-console.log(appConfig.secure('AWS_ACCESS_KEY_ID'), appConfig.secure('AWS_SECRET_KEY'));
-
-// Static Assets
-
-app.use(['/*.js', '/*.css'], function(request, result, next) {
-    if (request.acceptsEncodings(['gzip'])) {
-        var compressedVersion = __dirname + '/public' + request.originalUrl + ".gz";
-        try {
-            fs.accessSync(compressedVersion, fs.R_OK);
-            request.url += '.gz';
-            
-            result.header('Content-Encoding', 'gzip');
-            result.header('Content-Type', mime.lookup(path.extname(request.originalUrl)));
-        } catch(error) {
-            
-        }
+try {
+    if (!eval(appConfig.secure(activeVariable))) {
+        appIsActive = false;
     }
+} catch (e) {
+    appIsActive = false;
+}
+
+if (false === appIsActive) {
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'ejs');
+
+    app.disable('view cache');
     
-    result.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-    result.header('Expires', '-1');
-    result.header('Pragma', 'no-cache');
+    app.use('/', function(request, result, next) {
+        result.render('inactive');
+    });
     
-    next();
-});
+} else {
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, 'public')));
+    app.disable('view cache');
 
-app.use('/', function(request, result, next) {
-    result.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-    result.header('Expires', '-1');
-    result.header('Pragma', 'no-cache');
-    next();
-})
+    // uncomment after placing your favicon in /public
+    //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(cookieParser());
 
-app.use('/', indexRoute);
+    // Static Assets
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+    app.use(['/*.js', '/*.css'], function(request, result, next) {
+        if (request.acceptsEncodings(['gzip'])) {
+            var compressedVersion = __dirname + '/public' + request.originalUrl + ".gz";
+            try {
+                fs.accessSync(compressedVersion, fs.R_OK);
+                request.url += '.gz';
+                
+                result.header('Content-Encoding', 'gzip');
+                result.header('Content-Type', mime.lookup(path.extname(request.originalUrl)));
+            } catch(error) {
+                
+            }
+        }
+        
+        result.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+        result.header('Expires', '-1');
+        result.header('Pragma', 'no-cache');
+        
+        next();
+    });
 
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message
-  });
-});
+    app.use(express.static(path.join(__dirname, 'public')));
 
-// Connect to our database
+    app.use('/', function(request, result, next) {
+        result.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+        result.header('Expires', '-1');
+        result.header('Pragma', 'no-cache');
+        next();
+    })
 
-connectDb();
+    app.use('/', indexRoute);
+
+    // catch 404 and forward to error handler
+    app.use(function(req, res, next) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    });
+
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message
+        });
+    });
+
+    // Connect to our database
+
+    connectDb();
+}
 
 module.exports = app;
